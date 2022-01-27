bc404b9f7e6e2e395e47298efad6182b215251fa
Admin <carneiro.carolina@yahoo.com.br>
2022-01-27 15:39:17 +0100

    updating doc

--- FunctionGrowth.java
+++ FunctionGrowth.java
@@ -1,3 +1,10 @@
+/*
+1.3 Conditionals and Loops
+Write a program FunctionGrowth that prints a table of the values 
+logJV, JV, JVlogJV, JV2, JV3, and 2N for JV= 16,32,64,..., 2048. Usetabs (\t characters)
+to line up columns.
+*/
+
 public class FunctionGrowth {
     public static void main(String[] args) {
         System.out.println("log N" + "\t" + "N" + "\t" + "N log N" + "\t" + "N^2" + "\t" + "N^3" + "\t" + "2^N" + "\t");
